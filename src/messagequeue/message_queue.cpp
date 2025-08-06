c15401de3f813f5f6d3a8dce3856221ac98b217e
bibin <bibincnair1@gmail.com>
2025-08-06 19:59:23 +0200

    Reorganize MessageQueue methods for improved readability and remove unused CLI tests

--- src/messagequeue/message_queue.cpp
+++ src/messagequeue/message_queue.cpp
@@ -1,23 +1,17 @@
 #include "messagequeue/message_queue.h"
 
-void MessageQueue::push(std::unique_ptr<Message> msg) {
-    m_queue.push_back(std::move(msg));
+bool MessageQueue::empty() const { return m_queue.empty(); }
+
+size_t MessageQueue::size() const { return m_queue.size(); }
+
+std::optional<std::unique_ptr<Message>> MessageQueue::try_pop()
+{
+  if (m_queue.empty()) { return std::nullopt; }
+  // Considering that m_queue front and pop_front provide noexcept
+  // A subtle issue of move, exception and left with a dangling unique_ptr exception wont happen.
+  auto output = std::move(m_queue.front());
+  m_queue.pop_front();
+  return output;
 }
 
-std::optional<std::unique_ptr<Message>> MessageQueue::try_pop() {
-    if (m_queue.empty()) {
-        return std::nullopt;
-    }
-    
-    auto msg = std::move(m_queue.front());
-    m_queue.pop_front();
-    return msg;
-}
-
-size_t MessageQueue::size() const {
-    return m_queue.size();
-}
-
-bool MessageQueue::empty() const {
-    return m_queue.empty();
-}
+void MessageQueue::push(std::unique_ptr<Message> msg) { m_queue.push_back(std::move(msg)); }
